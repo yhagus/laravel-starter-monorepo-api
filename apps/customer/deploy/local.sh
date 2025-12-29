81926ea924f81f06c54a791920b452666d5f96d2
bagus <bagus.pranowo@vnt.co.id>
2025-12-29 09:25:42 +0700

    setup .gitignore and setup first-time development stage

--- apps/customer/deploy/local.sh
+++ apps/customer/deploy/local.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+if [ ! -f .env ]; then
+  cp .env.example .env
+  composer artisan:customer key:generate
+fi
+
 composer install --no-interaction --optimize-autoloader
+composer update --no-interaction --optimize-autoloader
 composer artisan:customer octane:frankenphp --host=0.0.0.0 --watch
