27e32345d68a73cd62a0ae444fada4404994f0dd
bagus <bagus.pranowo@vnt.co.id>
2026-01-02 11:33:37 +0700

    chore: use php exec instead of composer to prevent 300s lock time

--- apps/admin/deploy/local.sh
+++ apps/admin/deploy/local.sh
@@ -3,6 +3,4 @@ if [ ! -f .env ]; then
   cp .env.example .env
   composer artisan:customer key:generate
 fi
-composer install --no-interaction --optimize-autoloader
-composer update --no-interaction --optimize-autoloader
-composer artisan:admin octane:frankenphp --host=0.0.0.0 --watch
+exec php apps/admin/artisan octane:frankenphp --host=0.0.0.0 --watch
--- apps/customer/deploy/local.sh
+++ apps/customer/deploy/local.sh
@@ -3,7 +3,4 @@ if [ ! -f .env ]; then
   cp .env.example .env
   composer artisan:customer key:generate
 fi
-
-composer install --no-interaction --optimize-autoloader
-composer update --no-interaction --optimize-autoloader
-composer artisan:customer octane:frankenphp --host=0.0.0.0 --watch
+exec php apps/customer/artisan octane:frankenphp --host=0.0.0.0 --watch
